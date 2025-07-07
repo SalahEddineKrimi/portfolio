94911ce341721bb6f3f4f4c7a574a32a355e0f0b
Salah-Eddine Krimi <salah-eddine.krimi.1@ens.etsmtl.ca>
2025-07-07 17:16:56 +0100

    Eportfolio_V017

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,7 +65,7 @@ const Hero = () => {
             </div>
             <div className="flex items-center gap-3">
               <MapPin className="w-5 h-5 text-blue-300" />
-              <span>Sidi Boughoufala - Ouargla</span>
+              <span>Algérie</span>
             </div>
           </div>
 
